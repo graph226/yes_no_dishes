edb532e721d7830391f3d16624b90d880d703cfc
Shintaro Inuzuka <graph226@gmail.com>
2016-02-21 17:37:36 +0900

    Add a new table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212011032) do
+ActiveRecord::Schema.define(version: 20160221065629) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "dishes", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "question_table_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "items", force: :cascade do |t|
@@ -25,6 +32,12 @@ ActiveRecord::Schema.define(version: 20160212011032) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
